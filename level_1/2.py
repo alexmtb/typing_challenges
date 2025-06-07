6e079f8f5e2f179091220bb93b41b3137c1e4a2b
Alex Smirnov <aleximtb@gmail.com>
2025-06-07 21:43:40 +0300

    [UPDATE] Level_1 excercises completed

--- level_1/2.py
+++ level_1/2.py
@@ -1,9 +1,8 @@
-from constants import ___
+# from constants import ___
 
 
-def is_adult(age: ___, country_name: ___) -> ___:
-    pass
-
+def is_adult(age: int, country_name: str) -> bool:
+    return country_name == "Russia" and age >= 16
 
 if __name__ == "__main__":
     assert is_adult(age=17, country_name="Russia") is True
