1393dfa1bd0d290a3f015e77547e19c2f86a5815
Alex Smirnov <aleximtb@gmail.com>
2025-06-14 15:03:15 +0300

    [ADD] Level_2 is done

--- level_2/1.py
+++ level_2/1.py
@@ -1,9 +1,11 @@
-from constants import ___
+# from constants import ___
 
 
-def get_avg_currency_rate(rates_history: ___) -> ___:
-    pass
-
+def get_avg_currency_rate(rates_history: list[float]) -> float:
+    '''Calculate the average currency rate from a list of rates.'''
+    if rates_history:
+        return round(sum(rates_history) / len(rates_history), 1)
+    return 0.0
 
 if __name__ == "__main__":
     assert get_avg_currency_rate(rates_history=[30.2, 31.6, 29.0]) == 30.3
